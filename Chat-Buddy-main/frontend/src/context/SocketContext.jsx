c4a2c20ac5f93f212bab6f9c78100d71e77256ef
Muskan Duwesh <themuskanduwesh@gmail.com>
2024-10-06 06:30:12 +0530

    third commit

--- Chat-Buddy-main/frontend/src/context/SocketContext.jsx
+++ Chat-Buddy-main/frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(()=>{
         if(authUser) {
-            const socket = io("http://localhost:3000",{
+            const socket = io("https://chatapp2-0m90.onrender.com",{
                 query:{
                     userId : authUser._id,
                 },
